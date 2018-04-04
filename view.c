765b52018e307be65eab4faada825156e960be39
nclozza <nclozza@itba.edu.ar>
2018-04-04 03:14:39 -0300

    Now we can write and read from the shared memory, still needing some semaphores

--- view.c
+++ view.c
@@ -1,86 +1,38 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <fcntl.h>
+#include <sys/shm.h>
 #include <sys/stat.h>
-#include <semaphore.h>
 #include <sys/mman.h>
-
-void viewProcess(char* requestedPid)
+ 
+int main()
 {
-	sem_t *semRead,*semCanEnd;
-	char sharedMemName[32],semRead[32],semEndSignal[32];
+	// LU/CONY/FEDE AGREGAR LO DEL SEMAFORO
 
-	sprintf(sharedMemName,"/view%s",requestedPid);
-	sprintf(semRead,"/viewRead%s",requestedPid);
-	sprintf(semEndSignal,"/viewEnd%s",requestedPid);
-
-	semRead = sem_open(semRead,0);
-	semCanEnd = sem_open(semEndSignal,0);
-	if(semRead == SEM_FAILED || semCanEnd == SEM_FAILED)
-	{
-		perror("Error opening POSIX semaphore");
-		return;
-	}
-
-	int fileDescriptor;
-	fileDescriptor = shm_open(sharedMemName,O_RDWR,0777);
-	if(fileDescriptor == -1)
-	{
-		perror("Error opening shared memory space");
-		return;
-	}
-
-	struct stat sb;
-	fstat(fileDescriptor,&sb);
-
-	void * sharedMem = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fileDescriptor, 0);
-	if(sharedMem == MAP_FAILED)
-	{
-		perror("Error creating new mapping in the virtual adress of the calling process");
-		return;
-	}
-	char * c = (char *) sharedMem;
-	while(*c != EOF)
-	{
-		sem_wait(semRead);
-		while(*c != 0)
-		{
-			printf("%c\n", *c);
-			c++;
-		}
-		c++;
-	}
-
-	int closed = sem_close(semRead);
-	if(closed == -1)
-	{
-		perror("Error closing semaphore");
-		return;
-	}
-	munmap(sharedMem,sb.st_size);
-	//close(fd);
-
-	sem_post(semCanEnd);
-	int closedEnd = sem_close(semCanEnd);
-	if(closedEnd == -1)
-	{
-		perror("Error closing semaphore");
-		return;
-	}
-}
-
-
-
-
-int main(int argc, char const *argv[])
-{
-	if(argc != 2)
-	{
-		printf("Usage: ./view pid\n");
-	}
-
-	char* requestedPid = argv[1];
-	viewProcess(requestedPid);
-	return 0;
+    /* the size (in bytes) of shared memory object */
+    const int SIZE = 4096;
+ 
+    /* name of the shared memory object */
+	// LU/CONY/FEDE CAMBIAR ESTE NOMBRE
+    const char* name = "OS";
+ 
+    /* shared memory file descriptor */
+    int shm_fd;
+ 
+    /* pointer to shared memory object */
+    void* ptr;
+ 
+    /* open the shared memory object */
+    shm_fd = shm_open(name, O_RDONLY, 0666);
+ 
+    /* memory map the shared memory object */
+    ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+ 
+    /* read from the shared memory object */
+	// LU/CONY/FEDE ANTES DE LEER DE LA MEMORIA TIENE QUE CHECKEAR LO DEL SEMAFORO
+    printf("%s", (char*)ptr);
+ 
+    /* remove the shared memory object */
+    //shm_unlink(name);
+    return 0;
 }
